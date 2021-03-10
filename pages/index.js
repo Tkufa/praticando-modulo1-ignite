58a4dfe5afdf4df0008b0e6e8dd92bc93b0e4a94
Thiago Kufa <thiago.kufa@gmail.com>
2021-03-10 19:13:42 -0300

    primeira duvida!

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import HelloWorld from '../components/hello-world'
+import HomeSlider from "../components/HomeSlider";
 
 export default function Home() {
   return (
     <div className="app">
-      <HelloWorld />
+      <HomeSlider />
     </div>
   )
 }
